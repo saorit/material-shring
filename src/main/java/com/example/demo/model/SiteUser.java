c2ec66079a85f0413e3e8d96862b5d0488f1561d
tadokoro <tsst7163@yahaoo.co.jp>
2021-05-21 23:23:32 +0900

    Customize error messages

--- src/main/java/com/example/demo/model/SiteUser.java
+++ src/main/java/com/example/demo/model/SiteUser.java
@@ -19,12 +19,12 @@ public class SiteUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Size(min = 2, max = 20)
+   
+    @Size(min = 2, max = 20, message="ユーザー名は2文字から20文字内で入力してください")
     @UniqueLogin
     private String username;
 
-    @Size(min = 4, max = 255)
+    @Size(min = 4, max = 255, message="パスワードは4文字以上で入力してください")
     private String password;
     
 	
