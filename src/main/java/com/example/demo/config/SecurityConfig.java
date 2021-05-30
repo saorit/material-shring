50310e96c514b1832aacde1e935ee33ec4109852
tadokoro <tsst7163@yahaoo.co.jp>
2021-05-30 23:43:57 +0900

    fix config admin

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.example.demo.service.impl.UserDetailsServiceImpl;
@@ -22,36 +23,66 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	UserDetailsServiceImpl userDetailsService;
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		// 「/js/**」を追加
+		// spring securityで無視するリクエストパスを設定
+		// 静的ファイルを除外する
 		web.ignoring().antMatchers("/js/**", "/css/**", "/webjars/**");
 	}
+	
 
+	/**
+	 * 認証方法を設定する.
+	 *
+	 * @param auth AuthenticationManagerBuilder
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// 拡張してDB認証を実行する
+		auth.userDetailsService(service);
+	}
+	
+	/**
+	 * ユーザー情報Serviceクラス(実装クラス).
+	 */
+	@Autowired
+	UserDetailsServiceImpl service;
+
+	/**
+	 * パスワードの暗号化クラスを返却する.
+	 *
+	 * @return BCryptPasswordEncoder
+	 */
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+
+
+	
+	/**
+	 * 認証対象やログイン・ログアウト処理を設定する.
+	 *
+	 * @param http HttpSecurity
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		// ログイン処理の認証ルールを設定
 		http.authorizeRequests()
-				// 認証不要でアクセス可能
-				.antMatchers("/login", "/register", "/userconfirm").permitAll().
-				anyRequest().authenticated()
+				.antMatchers("/login", "/register", "/userconfirm").permitAll() // 認証なしでアクセス可能なパス
+				// ADMINロールを持つユーザーのみアクセス許可
+				.antMatchers("/admin/**").hasAuthority("ADMIN")
+				.anyRequest().authenticated() // それ以外は認証が必要
 				.and().sessionManagement().sessionFixation().none().and().formLogin().loginPage("/login")
-				.loginProcessingUrl("/index") // ログインフォームのアクションに指定したURL[action="@{/login}"]を設定
+				.loginProcessingUrl("/login") // ログインフォームのアクションに指定したURL[action="@{/login}"]を設定
 				.usernameParameter("username") // ログインフォームのユーザー欄のname属性を設定
 				.passwordParameter("password") // ログインフォームのパスワード欄のname属性を設定
-				.successForwardUrl("/") // ログイン成功時に遷移するURL
+				.successForwardUrl("/success") // ログイン成功時に遷移するURL
 				.failureUrl("/login?error") // ログイン失敗時に遷移するURL
 				.permitAll().and().logout().logoutUrl("/logout").permitAll()
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// userDetailsServiceを使用して、DBからユーザを参照できるようにします
-		auth.userDetailsService(userDetailsService);
-	}
 }
--- src/main/java/com/example/demo/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserDetailsServiceImpl.java
@@ -1,45 +1,82 @@
 package com.example.demo.service.impl;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.model.SiteUser;
 import com.example.demo.model.impl.UserDetailsImpl;
 import com.example.demo.repository.SiteUserRepository;
+import com.example.demo.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+/**
+ * ユーザー情報を取得するクラス. また、ユーザー情報の取得と合わせて、認証処理を行います。
+ */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	/**
+	 * User(Entity)クラスのリポジトリクラス.
+	 */
 	@Autowired
-    private final SiteUserRepository userRepository;
+	private SiteUserRepository repository;
+	
+	@Autowired
+	private UserService userService;
 
-    @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
+	/**
+	 * コンストラクタ.
+	 *
+	 * @param repository UserRepository
+	 */
+	@Autowired
+	public UserDetailsServiceImpl(SiteUserRepository repository) {
+		this.repository = repository;
+	}
 
-        SiteUser user = userRepository.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException(username + " not found");
-        }
-        return createUserDetails(user);
-    }
+	/**
+	 * ユーザー名に紐づくユーザーの詳細情報を取得する.
+	 *
+	 * @param username ユーザー名
+	 * @return ユーザーの詳細情報
+	 */
+	@Transactional
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-    public UserDetailsImpl createUserDetails(SiteUser user) {
-        Set<GrantedAuthority> grantedAuthories = new HashSet<>();
-        grantedAuthories.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+		// ユーザー名が空か判定
+		if (StringUtils.isEmpty(username)) {
+			// 空の場合、UsernameNotFoundExceptionをthrowする
+			throw new UsernameNotFoundException("Username is empty");
+		}
 
-        return new UserDetailsImpl(user, grantedAuthories);
-    }
+		// ユーザー名に紐づく情報を取得
+		SiteUser loginUser = userService.findOneUsername(username);
+
+		if (loginUser == null) {
+			// ユーザー情報が空の場合、UsernameNotFoundExceptionをthrowする
+			throw new UsernameNotFoundException("Not found Username : " + username);
+		}
+
+		// 権限情報
+		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+		// ユーザーの詳細情報のロールを権限情報に追加
+		authorities.add(new SimpleGrantedAuthority(loginUser.getRole()));
+
+		UserDetailsImpl user = new UserDetailsImpl(loginUser, authorities);
+
+		return user;
+	}
 }
